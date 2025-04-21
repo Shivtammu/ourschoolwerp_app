c0c129401495c0bca343bbad543be6d546f4bd73
santosh kumar <santosh.developer98@gmail.com>
2025-04-22 00:32:57 +0530

    inital development

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+  categories = [
+    { label: 'Fees', icon: 'cash' },
+    { label: 'Online Exams', icon: 'clipboard' },
+    { label: 'Digital Content', icon: 'film' },
+    { label: 'Online Classes', icon: 'desktop' },
+    { label: 'Circulars', icon: 'megaphone' },
+    { label: 'Support', icon: 'help-circle' },
+    { label: 'Menu', icon: 'restaurant' },
+    { label: 'Progress Report', icon: 'stats-chart' },
+    { label: 'Transport', icon: 'bus' },
+    { label: 'Settings', icon: 'settings' },
+    { label: 'Attendance', icon: 'checkmark-done' },
+    { label: 'Calendar', icon: 'calendar' },
+    { label: 'Profile', icon: 'person' },
+    { label: 'Exam Timetable', icon: 'calendar-number' },
+    { label: 'Time Table', icon: 'time' }
+  ];
+  constructor() {}
+
+}
